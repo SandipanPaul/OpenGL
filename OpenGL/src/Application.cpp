acd22b96dd5162f517c3c4ef3fdfa80d2de882f7
Rahul <paul.sandipan04@gmail.com>
2022-04-15 17:16:35 +0530

    Unforms implemented

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -123,6 +123,8 @@ int main(void)
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+    glfwSwapInterval(1);
+
     if (glewInit() != GLEW_OK)
         std::cout << "Error" << std::endl;
 
@@ -153,20 +155,30 @@ int main(void)
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW);
 
-
     ShaderProgramSource source = parseShader("res/shaders/Basic.shader");
-   
     unsigned int shader = CreateShader(source.VertexSource,source.FragmentSource);
-    glUseProgram(shader);
+    GLCall(glUseProgram(shader));
 
+    GLCall(int location = glGetUniformLocation(shader, "u_Color"));
+    ASSERT(location != -1);
+
+    GLCall(glUniform4f(location, 0.2f, 0.3f, 0.8f, 0.1f));
+
+    float r = 0.0f;
+    float increment = 0.05f;
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        GLCall(glDrawElements(GL_TRIANGLES,6,GL_INT,nullptr));
-
+        GLCall(glUniform4f(location, r, 0.3f, 0.9f, 0.1f));
+        GLCall(glDrawElements(GL_TRIANGLES,6,GL_UNSIGNED_INT,nullptr));
+        if (r > 1.0f)
+            increment = -0.05f;
+        else if (r < 0.0f)
+            increment = 0.05f;
+        r += increment;
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
 
